e9a3ef57e1b6091c8422f5613cebda54e6a4e63e
Atorretti <a_torreti@hotmail.com>
2023-09-14 02:07:36 +0100

    Alert
    Introduce alert pop up

--- js/app/main.js
+++ js/app/main.js
@@ -1,6 +1,7 @@
 require(['router'], function (router) {
     $(document).ready(function () {
         console.log('DOM is mounted and ready');
+        window.alert("Welcome and thank you for visiting my Pokedex. It may take a while to load all the content.");
        
         router.start();
 
